14c977dba3b2bb05bb397f33d28a6c0867c5594d
kylanhurt <kylan.hurt@gmail.com>
2019-10-26 21:41:01 -0500

    Enable front-end uploading of CSV

--- crud-react-laravel/src/ResultsList.js
+++ crud-react-laravel/src/ResultsList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Table } from 'semantic-ui-react'
 
+const API_URL = 'http://127.0.0.1:8000/api'
+
 class ResultsList extends Component {
     constructor(props) {
         super(props);
@@ -13,37 +15,61 @@ class ResultsList extends Component {
           .then(data => this.setState({ data: data.data }));
     }
 
+    onFileSelected = async (event) => {
+        console.log(event.target.files[0])
+        const data = new FormData()
+        data.append('file', event.target.files[0])
+        fetch(API_URL + '/people', {
+            method: 'POST',
+            body: data,
+            headers: {
+                'content-type': 'text/csv'
+            }
+        })
+    }
+
     render() {
         var data = this.state.data || [];
 
         return (
-            <Table celled padded>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell singleLine>First Name</Table.HeaderCell>
-                  <Table.HeaderCell>Last Name</Table.HeaderCell>
-                  <Table.HeaderCell>Email</Table.HeaderCell>
-                  <Table.HeaderCell>Status</Table.HeaderCell>
-                </Table.Row>
-              </Table.Header>
+            <div>
+                <div>
+                    <label htmlFor="peopleCSVUpload">Upload a people list:</label><br /><br />
+                    <input type="file"
+                        id="peopleCSVUpload" name="peopleCSVUpload"
+                        accept="text/csv"
+                        onChange={this.onFileSelected}
+                    />
+                </div>
+                <br />
+                <Table celled padded>
+                <Table.Header>
+                    <Table.Row>
+                    <Table.HeaderCell singleLine>First Name</Table.HeaderCell>
+                    <Table.HeaderCell>Last Name</Table.HeaderCell>
+                    <Table.HeaderCell>Email</Table.HeaderCell>
+                    <Table.HeaderCell>Status</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
 
-              <Table.Body>
+                <Table.Body>
 
-              {
-                  data.map((person, index) => {
-                      return (
-                          <Table.Row key={index}>
-                              <Table.Cell singleLine>{ person.first_name }</Table.Cell>
-                              <Table.Cell singleLine>{ person.last_name }</Table.Cell>
-                              <Table.Cell singleLine>{ person.email_address }</Table.Cell>
-                              <Table.Cell singleLine>{ person.status }</Table.Cell>
-                          </Table.Row>
-                      );
-                    })
-              }
+                {
+                    data.map((person, index) => {
+                        return (
+                            <Table.Row key={index}>
+                                <Table.Cell singleLine>{ person.first_name }</Table.Cell>
+                                <Table.Cell singleLine>{ person.last_name }</Table.Cell>
+                                <Table.Cell singleLine>{ person.email_address }</Table.Cell>
+                                <Table.Cell singleLine>{ person.status }</Table.Cell>
+                            </Table.Row>
+                        );
+                        })
+                }
 
-              </Table.Body>
-            </Table>
+                </Table.Body>
+                </Table>
+            </div>
     );
 }
 
