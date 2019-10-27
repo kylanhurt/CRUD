ae2831b69e5293b7b18900cc01ffaeabdc7846de
kylanhurt <kylan.hurt@gmail.com>
2019-10-26 23:45:26 -0500

    Initial setup for Groups model

--- crud-react-laravel/database/migrations/2019_10_27_041117_create_groups_table.php
+++ crud-react-laravel/database/migrations/2019_10_27_041117_create_groups_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePeopleTable extends Migration
+class CreateGroupsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class CreatePeopleTable extends Migration
      */
     public function up()
     {
-        Schema::create('people', function (Blueprint $table) {
+        Schema::create('groups', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('group_name');
             $table->timestamps();
         });
     }
@@ -26,6 +27,6 @@ class CreatePeopleTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('people');
+        Schema::dropIfExists('groups');
     }
 }
---
+++ crud-react-laravel/database/seeds/GroupTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GroupTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // initial group will be 'uncategorized' default
+        DB::table('groups')->insert([
+            'group_name' => 'uncategorized'
+        ]);
+        for ($i=0; $i < 10; $i++) {
+            DB::table('groups')->insert([
+                'group_name' => Str::random(10)
+            ]);
+        }
+    }
+}
